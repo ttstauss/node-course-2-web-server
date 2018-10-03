b2b271f6c142e3536d57989469eeef68d4af0158
Taylor <15224340+ttstauss@users.noreply.github.com>
2018-10-03 16:09:18 -0600

    Add project page

--- server.js
+++ server.js
@@ -47,6 +47,12 @@ app.get('/about', (req, res) => {
   })
 })
 
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects Page'
+  })
+})
+
 app.get('/bad', (req, res) => {
   res.send({
     error: 'Unable to handle request'
